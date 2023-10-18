a144dd0011e69ab3b375fa9401590af822d51457
Luis Gustavo Rodrigues Oliveira <lgrotrab@gmail.com>
2023-10-18 10:53:59 -0300

    Cria repositório para atividades simples de fundamentos da computação da PUCRS

---
+++ minibrowser/map.js
@@ -0,0 +1,7 @@
+let novoMapa = new Map();
+
+novoMapa.set("11/11/11", {});
+novoMapa.set("11/11/11", {});
+novoMapa.set("11/11/11", {});
+novoMapa.set("12/11/11", {});
+console.log(novoMapa);
---
+++ minibrowser/set.js
@@ -0,0 +1,10 @@
+let novoSet = new Set();
+
+novoSet.add("teste");
+novoSet.add("teste");
+novoSet.add("teste");
+novoSet.add("teste");
+novoSet.add("teste");
+novoSet.add("teste");
+novoSet.add("teste2");
+console.log(novoSet);
